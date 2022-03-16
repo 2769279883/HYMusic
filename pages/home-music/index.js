de55cdd921c043884e006d06764dc7c7c2b5aebf
2769279883 <2769279883@qq.com>
2022-03-16 23:56:09 +0800

    ds

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,21 +1,50 @@
 // pages/home-music/index.js
+import {getBanners} from "../../service/api_music"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    swiperHeight: 0,
+    banners: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'http://123.207.32.32:9001/top/mv',
+    this.getPageData()
+    // wx.request({
+    //   url: 'http://123.207.32.32:9001/top/mv',
+    // })
+  },
+
+
+  // 网络请求
+  getPageData: function () {
+    getBanners().then(res => {
+      this.setData({banners: res.banners})
     })
   },
+
+  // 事件处理
+  handleSearchClick: function () {
+    wx.navigateTo({
+      url: '/pages/detail-search/index',
+    })
+  },
+
+  swiperImageLoaded: function () {
+    // 获取图片的高度（如何去获取一个组件的高度）
+    const query = wx.createSelectorQuery()
+    query.select('.swiper-image').boundingClientRect()
+    // query.selectViewport().scrollOffset()
+    query.exec(res => {
+      const rect = res[0]
+      this.setData({swiperHeight: rect.height})
+    })
+  }
 
  
 })
